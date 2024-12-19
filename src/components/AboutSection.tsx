24a013599a811a2230931001ea121e654a95b2eb
Theadedamola <adedamolajose@gmail.com>
2024-12-19 21:31:30 +0100

    replace placeholder images in AboutSection with local image assets

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,4 +1,7 @@
 import { motion } from 'framer-motion'
+import image1 from './image 1.jpeg'
+import image2 from './image 2.jpg'
+import image3 from './image 3.jpg'
 
 export const AboutSection = () => {
   return (
@@ -20,17 +23,17 @@ export const AboutSection = () => {
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-8">
         <img
-          src="https://img.freepik.com/free-photo/close-up-young-man-having-meeting_23-2149060254.jpg?t=st=1734607916~exp=1734611516~hmac=9d49aa1767cb3e51f8eba88821c7484836534339997e5e5fbd68d84c42a931bd&w=740"
+          src={image3}
           alt=""
           className="h-full md:row-span-2 rounded-xl"
         />
         <img
-          src="https://img.freepik.com/free-photo/man-sitting-down-office-job-interview-desk-with-his-employers_23-2149034553.jpg?t=st=1734607595~exp=1734611195~hmac=087456a89e100696cd75382514a4b6b6b5e9524b1f220d2679a6da5f937a4f50&w=1480"
+          src={image2}
           alt=""
           className="h-full rounded-xl"
         />
         <img
-          src="https://img.freepik.com/free-photo/male-employee-getting-used-his-new-office-job-along-with-female-colleagues_23-2149034607.jpg?t=st=1734607946~exp=1734611546~hmac=93b83e1383039870d2e901ca7a6c81c4752ac9c28735d0d40eccc6fadca230e4&w=1480"
+          src={image1}
           alt=""
           className="h-full rounded-xl"
         />
